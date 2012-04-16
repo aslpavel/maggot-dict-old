a197dda0117c3ab554d406f9b88f0d827af12de8
aslpavel <asl.pavel@gmail.com>
2012-04-16 11:21:16 +0400

    [Dictionary] changed: better (un)bind

--- MaggotDict/dictionary.py
+++ MaggotDict/dictionary.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 import sys
 import json
+import itertools
 from os import path
 from collections import Mapping
 
@@ -24,12 +25,12 @@ __all__ = ('Dictionary',)
 #------------------------------------------------------------------------------#
 class Dictionary (Mapping):
     def __init__ (self, storage_file = None, logger = None):
-        # init logger
-        self.log = Log ('dictionary')
+        self.log = Log ('dict')
+        self.disposable = CompositeDisposable ()
+
+        # attatch logger
         if logger is not None:
             self.log.Subscribe (logger)
-
-        self.disposable = CompositeDisposable ()
 
         # open storage
         if storage_file is None:
@@ -40,54 +41,50 @@ class Dictionary (Mapping):
         # open config
         self.config = SackConfig (self.storage, CELL_CONFIG, lambda: {
             'dict_path' : [path.join (xdg.xdg_data_home, 'maggot-dict')],
-            'last_uid' : 0,
-            'providers' : {}, # name -> uid, last_modified
+            'providers' : {}, # name -> uid
         })
 
         # open index
         self.index = BPTree (SackProvider (self.storage, order = 256, type = 'PP', cell = CELL_INDEX,
             flags = SackProvider.FLAG_COMPRESSION))
 
-        # discover providers
+        # providers
         self.providers = {}
         updated = False
         for provider_type in providers.All:
             for dict_path in self.config.dict_path:
                 for provider in provider_type.Discover (dict_path):
                     self.disposable += provider
-                    info = getattr (self.config.providers, provider.Name, None)
-                    if info is None:
-                        # allocate uid
-                        uid, self.config.last_uid = self.config.last_uid, self.config.last_uid + 1
-                        self.providers [uid] = provider
 
-                        # build index
-                        count = 0
-                        with self.log.Progress (provider.Name) as progress:
-                            progress ('0')
-                            for word, desc in provider:
-                                count += 1
-                                if not count % 100:
-                                    progress (str (count))
-                                record = self.index.get (word)
-                                if record is None:
-                                    record = []
-                                record.append ((uid, desc))
-                                self.index [word] = record
-                            self.index.provider.Flush ()
-
-                        # update config
-                        setattr (self.config.providers, provider.Name, [uid, provider.LastModified])
+                    # get config
+                    config = self.config.providers.Get (provider.Name, None)
+                    if config is None:
+                        config = self.bind (provider)
                         updated = True
-                    else:
-                        uid, last_modified = info
-                        if last_modified < provider.LastModified:
-                            print (':: TODO: rebuild index')
-                            pass
-                        else:
-                            self.providers [uid] = provider
+
+                    # update mapping
+                    self.providers [config.uid] = provider
+
+        # clean index
+        uids = []
+        for name, config in self.config.providers.Items ():
+            if config.uid not in self.providers:
+                uids.append (config.uid)
+        if uids:
+            self.unbind (uids)
+            updated = True
+
+        # flush storage
         if updated:
+            self.index.provider.Flush ()
             self.config.Flush ()
+
+    #--------------------------------------------------------------------------#
+    # Properties                                                               #
+    #--------------------------------------------------------------------------#
+    @property
+    def Log (self):
+        return self.log
 
     #--------------------------------------------------------------------------#
     # Mapping Interface                                                        #
@@ -104,6 +101,65 @@ class Dictionary (Mapping):
             return (Entry (self, word, record) for word, record in self.index [word])
         else:
             return Entry (self, word, self.index [word])
+
+    #--------------------------------------------------------------------------#
+    # Private                                                                  #
+    #--------------------------------------------------------------------------#
+    def bind (self, provider):
+        # allocate uid
+        uid_mask = 0
+        for name, config in self.config.providers.Items ():
+            uid_mask |= 1 << config.uid
+        for uid in itertools.count ():
+            if not ((1 << uid) & uid_mask):
+                break
+
+        # update index
+        count = 0
+        with self.log.Progress (provider.Name) as progress:
+            progress ('0')
+
+            for word, desc in provider:
+                record = self.index.get (word)
+                if record is None:
+                    record = {}
+                record [uid] = desc
+                self.index [word] = record
+
+                # update progress
+                count += 1
+                if count % 100 == 0:
+                    progress (str (count))
+
+        # update config
+        self.config.providers [provider.Name] = {'uid' : uid}
+
+        return self.config.providers [provider.Name]
+
+    def unbind (self, uids):
+        with self.log.Pending ('unbinding uids: {}'.format (', '.join (str (uid) for uid in uids))):
+            # update index
+            empty = []
+            for entry in self:
+                desc = None
+                for uid in uids:
+                    desc = entry.record.pop (uid, None)
+                if desc is not None:
+                    if not entry:
+                        empty.append [entry.Word]
+                    else:
+                        self.index [entry.Word] = entry.record
+
+            for word in empty:
+                del self.index [word]
+
+            # update config
+            names = []
+            for name, config in self.config.providers.Items ():
+                if config.uid in uids:
+                    names.append (name)
+            for name in names:
+                del self.config.providers [name]
 
     #--------------------------------------------------------------------------#
     # Dispose                                                                  #
@@ -133,7 +189,7 @@ class Entry (object):
         return self.word
 
     def __iter__ (self):
-        for uid, desc in self.record:
+        for uid, desc in self.record.items ():
             provider = self.owner.providers [uid]
             yield provider.Name, provider [desc]
 
